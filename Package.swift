ebe33e4e731bb3fe7b5de33a9935a0b648d63661
LDNHRH <ethanmofokeng@yahoo.com>
2025-11-30 15:38:51 -0500

    update firebase package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // package(url: /* package url */, from: "1.0.0")
-        .package(url: "https://github.com/firebase/firebase-ios-sdk",from: "11.0.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk",from: "12.6.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
